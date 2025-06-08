dc2da1278f33f9ad54ca509357e80bd1e498ceab
LYNRmilktea <billy920517@gmail.com>
2025-06-09 04:00:13 +0800

    Add files via upload

---
+++ homework2/src/BST.cpp
@@ -0,0 +1,205 @@
+﻿#include <iostream>
+#include <utility>
+#include <stack>
+#include <random>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+template <class K, class E>
+class Dictionary {
+public:
+	virtual bool IsEmpty() const = 0;
+	virtual pair<K, E>* Get(const K&) = 0;
+	virtual void Insert(const pair<K, E>&) = 0;
+	virtual void Delete(const K&) = 0;
+};
+
+template <class K, class E>
+class BST : public Dictionary<K, E> {
+private:
+	struct TreeNode {
+		pair<K, E> data;
+		TreeNode* leftChild;
+		TreeNode* rightChild;
+		int leftSize;
+		TreeNode(const pair<K, E>& thePair)
+			: data(thePair), leftChild(nullptr), rightChild(nullptr), leftSize(0) {
+		}
+	};
+
+	TreeNode* root;
+	pair<K, E>* Get(TreeNode*, const K&) const;
+	void Insert(TreeNode*&, const pair<K, E>&);
+	int calculateHeight(TreeNode* root) const;
+	TreeNode* Delete(TreeNode*&, const K&, bool& deletedLeft);
+
+public:
+	BST() : root(nullptr) {}
+	~BST();
+	bool IsEmpty() const { return root == nullptr; }
+	pair<K, E>* Get(const K&);
+	pair<K, E>* RankGet(int);
+	void Insert(const pair<K, E>&);
+	void Delete(const K&);
+	int Height() const { return calculateHeight(root); }
+	int Size(TreeNode*) const;
+	TreeNode* GetRoot() const { return root; }
+};
+
+template <class K, class E>
+BST<K, E>::~BST() {
+	stack<TreeNode*> s;
+	TreeNode* current = root;
+	while (current || !s.empty()) {
+		while (current) {
+			s.push(current);
+			current = current->leftChild;
+		}
+		current = s.top(); s.pop();
+		TreeNode* temp = current;
+		current = current->rightChild;
+		delete temp;
+	}
+}
+
+template <class K, class E>
+pair<K, E>* BST<K, E>::Get(const K& k) {
+	return Get(root, k);
+}
+
+template <class K, class E>
+pair<K, E>* BST<K, E>::Get(TreeNode* p, const K& k) const {
+	if (!p) return nullptr;
+	if (k < p->data.first) return Get(p->leftChild, k);
+	if (k > p->data.first) return Get(p->rightChild, k);
+	return &p->data;
+}
+
+template <class K, class E>
+pair<K, E>* BST<K, E>::RankGet(int r) {
+	TreeNode* currentNode = root;
+	while (currentNode) {
+		if (r < currentNode->leftSize) {
+			currentNode = currentNode->leftChild;
+		}
+		else if (r > currentNode->leftSize) {
+			r -= currentNode->leftSize + 1;
+			currentNode = currentNode->rightChild;
+		}
+		else {
+			return &(currentNode->data);
+		}
+	}
+	return nullptr;
+}
+
+template <class K, class E>
+void BST<K, E>::Insert(const pair<K, E>& thePair) {
+	TreeNode* p = root;
+	TreeNode* pp = nullptr;
+	while (p) {
+		pp = p;
+		if (thePair.first < p->data.first) {
+			p->leftSize++;
+			p = p->leftChild;
+		}
+		else if (thePair.first > p->data.first) {
+			p = p->rightChild;
+		}
+		else {
+			p->data.second = thePair.second;
+			return;
+		}
+	}
+	TreeNode* newNode = new TreeNode(thePair);
+	if (!root) root = newNode;
+	else if (thePair.first < pp->data.first) pp->leftChild = newNode;
+	else pp->rightChild = newNode;
+}
+
+template <class K, class E>
+void BST<K, E>::Delete(const K& k) {
+	bool deletedLeft = false;
+	root = Delete(root, k, deletedLeft);
+}
+
+template <class K, class E>
+typename BST<K, E>::TreeNode* BST<K, E>::Delete(TreeNode*& node, const K& k, bool& deletedLeft) {
+	if (!node) return nullptr;
+
+	if (k < node->data.first) {
+		node->leftChild = Delete(node->leftChild, k, deletedLeft);
+		if (deletedLeft) node->leftSize--;
+	}
+	else if (k > node->data.first) {
+		node->rightChild = Delete(node->rightChild, k, deletedLeft);
+	}
+	else {
+		deletedLeft = true;
+		if (!node->leftChild) {
+			TreeNode* temp = node->rightChild;
+			delete node;
+			return temp;
+		}
+		else if (!node->rightChild) {
+			TreeNode* temp = node->leftChild;
+			delete node;
+			return temp;
+		}
+		else {
+			TreeNode* s = node->leftChild;
+			TreeNode* sp = node;
+			while (s->rightChild) {
+				sp = s;
+				s = s->rightChild;
+			}
+			node->data = s->data;
+			bool dummy = false;
+			node->leftChild = Delete(node->leftChild, s->data.first, dummy);
+			node->leftSize--;
+		}
+	}
+	return node;
+}
+
+template <class K, class E>
+int BST<K, E>::Size(TreeNode* node) const {
+	if (!node) return 0;
+	return Size(node->leftChild) + Size(node->rightChild) + 1;
+}
+
+template <class K, class E>
+int BST<K, E>::calculateHeight(TreeNode* root) const {
+	if (!root) return 0;
+	int leftHeight = calculateHeight(root->leftChild);
+	int rightHeight = calculateHeight(root->rightChild);
+	return max(leftHeight, rightHeight) + 1;
+}
+
+int main() {
+	const int testSizes[] = { 100, 500, 1000, 2000, 3000, 4000, 5000, 10000 };
+	const int numTests = sizeof(testSizes) / sizeof(testSizes[0]);
+
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> dist(1, 1000000);
+
+	cout << left << setw(10) << "n" << setw(15) << "height" << setw(20) << "log2(n)" << setw(20) << "height / log2(n)" << endl;
+	cout << fixed << setprecision(4);
+
+	for (int i = 0; i < numTests; ++i) {
+		int n = testSizes[i];
+		BST<int, int> bst;
+		for (int j = 0; j < n; ++j) {
+			int key = dist(gen);
+			bst.Insert(make_pair(key, 0));
+		}
+		int height = bst.Height();
+		double log2n = log2(n);
+		double ratio = height / log2n;
+		cout << setw(10) << n << setw(15) << height << setw(20) << log2n << setw(20) << ratio << endl;
+	}
+
+	return 0;
+}
---
+++ homework2/src/minheap.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+using namespace std;
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+template <class T>
+class MinPQ {
+public:
+    virtual ~MinPQ() {}
+    virtual bool IsEmpty() const = 0;
+    virtual const T& Top() const = 0;
+    virtual void Push(const T& x) = 0;
+    virtual void Pop() = 0;
+};
+
+template <class T>
+class MinHeap : public MinPQ<T> {
+private:
+    T* heap;
+    int heapSize;
+    int capacity;
+
+public:
+    MinHeap(int theCapacity = 10);
+    ~MinHeap();
+    void Push(const T& e);
+    void Pop();
+    const T& Top() const;
+    bool IsEmpty() const { return heapSize == 0; }
+    int Size() const { return heapSize; }
+    void PrintHeap() const;
+};
+
+template <class T>
+void ChangeSize1D(T*& a, int oldSize, int newSize) {
+    if (newSize < 0) throw runtime_error("New length must be >= 0");
+    T* temp = new T[newSize + 1];
+    int number = min(oldSize, newSize);
+    copy(a, a + number + 1, temp);
+    delete[] a;
+    a = temp;
+}
+
+template <class T>
+MinHeap<T>::MinHeap(int theCapacity) {
+    if (theCapacity < 1) throw runtime_error("Capacity must be >= 1");
+    capacity = theCapacity;
+    heapSize = 0;
+    heap = new T[capacity + 1];
+}
+
+template <class T>
+MinHeap<T>::~MinHeap() {
+    delete[] heap;
+}
+
+template <class T>
+void MinHeap<T>::Push(const T& e) {
+    if (heapSize == capacity) {
+        ChangeSize1D(heap, capacity, 2 * capacity);
+        capacity *= 2;
+    }
+    int currentNode = ++heapSize;
+    while (currentNode != 1 && heap[currentNode / 2] > e) {
+        heap[currentNode] = heap[currentNode / 2];
+        currentNode /= 2;
+    }
+    heap[currentNode] = e;
+}
+
+template <class T>
+void MinHeap<T>::Pop() {
+    if (IsEmpty()) throw runtime_error("Heap is empty. Cannot delete");
+    T lastE = heap[heapSize--];
+    int currentNode = 1;
+    int child = 2;
+    while (child <= heapSize) {
+        if (child < heapSize && heap[child] > heap[child + 1]) child++;
+        if (lastE <= heap[child]) break;
+        heap[currentNode] = heap[child];
+        currentNode = child;
+        child *= 2;
+    }
+    heap[currentNode] = lastE;
+}
+
+template <class T>
+const T& MinHeap<T>::Top() const {
+    if (IsEmpty()) throw runtime_error("Heap is empty. No top element");
+    return heap[1];
+}
+
+template <class T>
+void MinHeap<T>::PrintHeap() const {
+    for (int i = 1; i <= heapSize; ++i)
+        cout << heap[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    MinHeap<int> minHeap(10);
+    minHeap.Push(10);
+    minHeap.Push(14);
+    minHeap.Push(17);
+    minHeap.Push(20);
+    minHeap.Push(30);
+    minHeap.Push(21);
+    minHeap.Push(44);
+
+    cout << "Pop and print in order:" << endl;
+    while (!minHeap.IsEmpty()) {
+        cout << minHeap.Top() << " ";
+        minHeap.Pop();
+    }
+
+
+    minHeap.Push(10);
+    minHeap.Push(14);
+    minHeap.Push(17);
+    minHeap.Push(20);
+    minHeap.Push(30);
+    minHeap.Push(21);
+    minHeap.Push(44);
+
+    cout << endl << "Printheap Memory for debug:" << endl;
+    minHeap.PrintHeap();
+
+    return 0;
+}
